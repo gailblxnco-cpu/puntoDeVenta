b138a8bfc9f5d4e3254a311e19bfcd19b28df062
gail blanco <gail.blxnco@gmail.com>
2026-04-26 16:34:03 -0600

    Initial commit

---
+++ src/main/java/abd/puntodeventa/InventarioController.java
@@ -0,0 +1,4 @@
+package abd.puntodeventa;
+
+public class InventarioController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module abd.puntodeventa {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens abd.puntodeventa to javafx.fxml;
+    exports abd.puntodeventa;
+}
